adf72050d6a9aca702d86bf18d2b1dba682a5447
rachel-0131 <rachel040131@gmail.com>
2025-08-18 23:36:20 +0900

    style(lint): lint, prettier 업데이트

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,52 @@
+/* eslint-disable quote-props */
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
-  root: true,
-  env: {
-    node: true,
-    jest: true,
-  },
-  ignorePatterns: ['.eslintrc.js'],
-  rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-  },
+    root: true,
+    env: {
+        node: true,
+        jest: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:import/errors',
+        'plugin:import/warnings',
+        'plugin:import/typescript',
+        'plugin:@typescript-eslint/recommended',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: ['tsconfig.json', 'tsconfig.dev.json'],
+        sourceType: 'module',
+        tsconfigRootDir: __dirname,
+    },
+    ignorePatterns: [
+        '/lib/**/*', // Ignore built files.
+    ],
+    plugins: ['@typescript-eslint/eslint-plugin'],
+    rules: {
+        '@typescript-eslint/no-unused-vars': [
+            'warn',
+            { varsIgnorePattern: '^_', argsIgnorePattern: '^_' },
+        ],
+        'object-curly-spacing': ['error', 'always'],
+        quotes: ['error', 'single'],
+        'import/no-unresolved': 0,
+        indent: 'off',
+        'require-jsdoc': 'off',
+        'valid-jsdoc': 'off',
+        'prefer-promise-reject-errors': 'off',
+        'space-before-function-paren': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        camelcase: 'off',
+        'new-cap': 'off',
+        'operator-linebreak': [
+            'error',
+            'after',
+            {
+                overrides: {
+                    ':': 'before',
+                    '?': 'before',
+                },
+            },
+        ],
+    },
 };
